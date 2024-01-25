928ed26fe4462994a28ab66241f7d7457bbc97da
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-01-25 23:24:26 +0300

    Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./layouts/Navbar";
+import Dashboard from "./layouts/Dashboard";
 
 function App() {
   return (
-<div>hello</div>
-  )
+    <div>
+      <Navbar />
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StyledEngineProvider } from '@mui/styled-engine';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <StyledEngineProvider injectFirst>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </StyledEngineProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
